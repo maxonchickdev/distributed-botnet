6236f433ebdf206b30c39ce359e94925dbecd90c
polinom <kutsenko.pn@ucu.edu.ua>
2024-02-28 23:01:18 +0200

    Added sqlite3  configuration | commit

--- cpp_server/main.cpp
+++ cpp_server/main.cpp
@@ -3,32 +3,64 @@
 
 #include <iostream>
 
+#include <crow.h>
+#include <crow/middlewares/cors.h>
+#include <cpr/cpr.h>
+#include <sqlite3.h>
 
-#include "crow.h"
-#include "include/crow/middlewares/cors.h"
-#include "include/cpr/cpr.h"
 
+void connection(sqlite3* db, sqlite3_stmt* stmt) {
+    int res_connecting;
+    if (sqlite3_open("demo.db", &db) == SQLITE_OK) {
+        res_connecting = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS user(name varchar(50), roll INT, email varchar(80));", 0, 0, 0);
+        
+        if (res_connecting != SQLITE_OK) {
+            std::cerr << "Error: " << sqlite3_errmsg(db) << std::endl;
+        }
+        else {
+            std::cout << "Table created successfully" << std::endl;
+        }
+        
+        sqlite3_close(db);
+    }
+    else {
+        std::cerr << "Failed to open database." << std::endl;
+    }
+}
 
-int main() {
+int main()
+{
+    sqlite3 *db;
+    sqlite3_stmt *stmt;
+    connection(db, stmt);
+
+    sqlite3_close(db);
+
     crow::App<crow::CORSHandler> app;
 
-    auto& cors = app.get_middleware<crow::CORSHandler>();
+    auto &cors = app.get_middleware<crow::CORSHandler>();
     cors
-      .global().headers("Origin", "Content-Type", "Accept", "*").methods("POST"_method, "GET"_method).prefix("/cors").origin("example.com").prefix("/nocors").ignore();
+        .global()
+        .headers("Origin", "Content-Type", "Accept", "*")
+        .methods("POST"_method, "GET"_method)
+        .prefix("/cors")
+        .origin("example.com")
+        .prefix("/nocors")
+        .ignore();
 
-    CROW_ROUTE(app, "/")([](){
-        cpr::Response r = cpr::Get(cpr::Url{"https://643c27d14477945573689436.mockapi.io/items"});
-        std::cout << r.status_code << std::endl;
-        return "Maxondevelop";
+    CROW_ROUTE(app, "/") ([]() {
+        cpr::Response r = cpr::Get(cpr::Url{"https://65df88a5ff5e305f32a26bdf.mockapi.io/destination/elements/"});
+        std::cout << r.text << std::endl;
+        return "Maxondevelop"; 
     });
 
-    CROW_ROUTE(app, "/hello/<int>")([](int count){
+    CROW_ROUTE(app, "/hello/<int>") ([](int count) { 
         return crow::response(std::to_string(count));
     });
 
     app.port(8080)
-    .multithreaded()
-    .run();
+        .multithreaded()
+        .run();
 
     return 0;
 }
--- front-end-test/src/services/Services.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const Services = {
-	async getData() {
-		const response = await axios.get('http://0.0.0.0:8080/')
-		return response.data
-	},
-}
