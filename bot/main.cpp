ffe5dddeb0c0def0479785a093c125b81b3bb888
polinom <kutsenko.pn@ucu.edu.ua>
2024-03-03 17:33:55 +0200

    Added correct read_from_db method && modified bot | commit

--- bot/main.cpp
+++ bot/main.cpp
@@ -2,16 +2,29 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 #include <iostream>
+#include <string>
 #include <cpr/cpr.h>
 
 int main(int argc, char* argv[]) {
-    cpr::Response r = cpr::Get(cpr::Url{"http://localhost:8080/url-to-bot/"}, 
-                        cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
-                        cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+    cpr::Response r = cpr::Get(cpr::Url{"http://localhost:8080/url-to-bot/"});
 
+    std::string URL = r.text;
+
+    std::cout << URL << std::endl;
     std::cout << r.status_code << std::endl;
-    std::cout << r.header["content-type"] << std::endl;
-    std::cout << r.text << std::endl;
+
+    int counter = 0;
+
+    for(int i = 0; i < 10; ++i) {
+        cpr::Response target_r = cpr::Get(cpr::Url{URL});
+        if(target_r.status_code != 200) {
+            break;
+        }
+        ++counter;
+        std::cout << "request " << i << " sending successfully" << std::endl;
+    }
+
+    std::cout << counter << std::endl;
 
     return 0;
 }
