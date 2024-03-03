707664984af5943aff81710de53e1fbeca71aa9c
polinom <kutsenko.pn@ucu.edu.ua>
2024-03-03 20:29:41 +0200

    Added doxygen files | commit

--- bot/main.cpp
+++ bot/main.cpp
@@ -1,9 +1,30 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
+/**
+ * @file main.cpp
+ * @author your name (kutsenko.pn@ucu.edu.ua)
+ * @brief This file handles bot wich sending requests
+ * @version 0.1
+ * @date 2024-03-03
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
 #include <iostream>
 #include <string>
 #include <cpr/cpr.h>
+
+
+/**
+ * @brief Main method
+ * 
+ * @param argc Number of params wich users using in CLI
+ * @param argv All params wich users using in CLI
+ * @return int 
+ */
 
 int main(int argc, char* argv[]) {
     cpr::Response r = cpr::Get(cpr::Url{"http://localhost:8080/url-to-bot/"});
