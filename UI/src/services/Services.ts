31640f7dc4e5b9fe719640dbc810e7f16be7b83f
polinom <kutsenko.pn@ucu.edu.ua>
2024-03-25 16:16:21 +0200

    Clean commit

--- UI/src/services/Services.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const Services = {
-	async pushUrl(url: string) {
-		const response = await axios.post<string>("http://localhost:8080/get-url/", url)
-		return response.data
-	}
-}
--- bot/includes/parse_config.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef PARSE_CONFIG_INCLUDE
-#define PARSE_CONFIG_INCLUDE
-
-#include <map>
-
-std::map<std::string, std::string> parse_config(const std::string &path_to_config);
-
-#endif // PARSE_CONFIG_INCLUDE
